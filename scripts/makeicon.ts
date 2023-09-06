6da9011db854dca56188d92d63a20249802eaab9
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2023-09-06 07:07:15 -0400

    better icon handling

---
+++ scripts/makeicon.ts
@@ -0,0 +1,28 @@
+import { IconParams, icon } from "@fortawesome/fontawesome-svg-core";
+import path from "path";
+import sharp from "sharp";
+
+const [theIcon, theFile] = process.argv.slice(2);
+
+if (!theIcon || !theFile) {
+    console.error("makeicon <icon> <file>");
+    process.exit(-1);
+}
+const bit = require("@fortawesome/sharp-solid-svg-icons")[theIcon];
+if (!bit) {
+    console.error(`invalid icon ${theIcon}`);
+    process.exit(-1);
+}
+
+const svg = icon(bit, {
+    styles: { color: "white" },
+}).html[0];
+
+sharp(Buffer.from(svg))
+    .resize({ width: 32, height: 32, fit: "contain", background: { r: 0, g: 0, b: 0, alpha: 0 } })
+    .png()
+    .toFile(path.resolve("hosted", "icons", `${theFile}.png`))
+    .then(() => {
+        console.log("done");
+    })
+    .catch((e) => console.error(e));
