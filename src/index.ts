72879378814a3902d2fc95c3b9995f0e7763b4e9
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2023-09-07 03:51:27 -0400

    name change on listable and more icons

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Accordian } from "./widgets/accordian";
-export { default as Listable } from "./widgets/listable";
+export { default as DynamicList } from "./widgets/dynamiclist";
 export { default as Modal } from "./widgets/modal";
 
 export const UIIcons = {
@@ -22,5 +22,13 @@ export const UIIcons = {
 
         backward: "https://raw.githubusercontent.com/RobMayer/ttpg-trh-ui/main/hosted/icons/media/backward.png",
         forward: "https://raw.githubusercontent.com/RobMayer/ttpg-trh-ui/main/hosted/icons/media/forward.png",
+    },
+    window: {
+        maximize: "https://raw.githubusercontent.com/RobMayer/ttpg-trh-ui/main/hosted/icons/window/maximize.png",
+        minimize: "https://raw.githubusercontent.com/RobMayer/ttpg-trh-ui/main/hosted/icons/window/minimize.png",
+        restore: "https://raw.githubusercontent.com/RobMayer/ttpg-trh-ui/main/hosted/icons/window/restore.png",
+        fullscreen: "https://raw.githubusercontent.com/RobMayer/ttpg-trh-ui/main/hosted/icons/window/fullscreen.png",
+        close: "https://raw.githubusercontent.com/RobMayer/ttpg-trh-ui/main/hosted/icons/actions/close.png",
+        open: "https://raw.githubusercontent.com/RobMayer/ttpg-trh-ui/main/hosted/icons/window/fullscreen.png",
     },
 } as const;
--- src/widgets/dynamiclist.tsx
+++ src/widgets/dynamiclist.tsx
@@ -1,7 +1,19 @@
 import { VerticalAlignment, TextJustification, VerticalBox } from "@tabletop-playground/api";
 import { jsxInTTPG, boxChild, useRef, JSXNode, render, RefObject } from "jsx-in-ttpg";
 
-const Listable = ({ title, populator, handle }: { title: string; populator: () => JSXNode | JSXNode[]; handle?: RefObject<{ refresh: () => void }> }) => {
+const Listable = ({
+    title,
+    populator,
+    handle,
+    buttonSize,
+    gap = 8,
+}: {
+    title: string;
+    populator: () => JSXNode | JSXNode[];
+    handle?: RefObject<{ refresh: () => void }>;
+    buttonSize?: number;
+    gap?: number;
+}) => {
     const listRef = useRef<VerticalBox>();
 
     const refresh = () => {
@@ -19,15 +31,15 @@ const Listable = ({ title, populator, handle }: { title: string; populator: () =
     }
 
     return (
-        <verticalbox gap={8} valign={VerticalAlignment.Fill}>
+        <verticalbox gap={gap} valign={VerticalAlignment.Fill}>
             <horizontalbox valign={VerticalAlignment.Center}>
                 {boxChild(1, <text justify={TextJustification.Center}>{title}</text>)}
-                <imagebutton height={24} url={"https://raw.githubusercontent.com/RobMayer/ttpg-trh-ui/main/hosted/icons/actions/refresh.png"} onClick={refresh} />
+                <imagebutton height={buttonSize} url={"https://raw.githubusercontent.com/RobMayer/ttpg-trh-ui/main/hosted/icons/actions/refresh.png"} onClick={refresh} />
             </horizontalbox>
             {boxChild(
                 1,
                 <border color={"r181818"}>
-                    <verticalbox gap={8} ref={listRef} valign={VerticalAlignment.Fill}>
+                    <verticalbox gap={gap} ref={listRef} valign={VerticalAlignment.Fill}>
                         {populator()}
                     </verticalbox>
                 </border>
