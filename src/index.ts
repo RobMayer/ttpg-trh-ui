e6448357dae33107072333b3d09f12be9e0e6cf7
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2023-10-27 19:27:51 -0400

    oops

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { default as icons } from "./icons";
 export { Accordian, AccordianRef, AccordianProps } from "./containers/accordian";
 export { Frame, FrameRef, FrameProps } from "./containers/frame";
 export { Dialog, DialogRef, DialogProps } from "./containers/dialog";
+export { Modal, ModalRef, ModalProps } from "./containers/modal";
 export { Tabs, TabsRef, TabsProps } from "./containers/tabs";
 
 export { DynamicList, DynamicListRef, DynamicListProps } from "./listers/dynamiclist";
